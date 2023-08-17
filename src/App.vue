1b0a7d8e005906edb255285495a42f1d116f5acd
ian shortridge <ianshortridge@gmail.com>
2023-08-17 00:22:45 -0400

    Added new stuff to do

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+        <mainPage msg="Pick your Poison"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mainPage from './components/main-page.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mainPage
   }
 }
 </script>
---
+++ src/components/main-page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-page">
+    add pets and shit here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main-page',
+  props: {
+    msg: String
+  }
+}
+</script>
