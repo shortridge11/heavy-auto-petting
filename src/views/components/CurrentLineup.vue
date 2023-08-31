2b1d8abff0b6ca67d1a3711b558a04c6c806f809
ian shortridge <ianshortridge@gmail.com>
2023-08-30 23:46:00 -0400

    Added new configs for scss files and bootstrap

---
+++ src/views/components/CurrentLineup.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="team-container">
+		<div class="row">
+			<div class="col-2">
+				<span> Pre Team Spacer </span>
+			</div>
+			<div class="col-1 animal-col">
+				<pet-display pet-type="Beaver" />
+			</div>
+			<div class="col-1 animal-col">
+				<pet-display pet-type="Monkey" />
+			</div>
+			<div class="col-1 animal-col">
+				<pet-display pet-type="Crocodile" />
+			</div>
+			<div class="col-1 animal-col">
+				<pet-display pet-type="Whale" />
+			</div>
+			<div class="col-1 animal-col">
+				<pet-display pet-type="Deer" />
+			</div>
+			<div class="col-5">
+				<span> Post Team Spacer </span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import PetDisplay from  './components/PetDisplay.vue'
+
+export default {
+  name: 'CurrentLineup',
+  props: {
+    msg: String
+  },
+  components: {
+	PetDisplay
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
